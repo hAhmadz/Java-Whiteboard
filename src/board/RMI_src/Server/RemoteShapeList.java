bc6a8fa955ea556c3d7c1587a2ca84c1dae2d4d3
dripper1 <dripper@student.unimelb.edu.au>
2017-09-27 23:41:20 +1000

    Basic RMI implementation of client/server

---
+++ src/board/RMI_src/Server/RemoteShapeList.java
@@ -0,0 +1,20 @@
+package Server;
+
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import Misc.ColoredShape;
+
+
+public interface RemoteShapeList extends Remote {
+
+	//ArrayList<ColoredShape> shapes = new ArrayList<ColoredShape>();
+	public void addColoredShape(ColoredShape shape)  throws RemoteException;
+	//public void update();
+	public void clear()  throws RemoteException;
+	
+	public ArrayList<ColoredShape> getShapes()  throws RemoteException;
+	
+}
---
+++ src/board/RMI_src/Server/Server.java
@@ -0,0 +1,23 @@
+package Server;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Server {
+
+	public static void main(String args[]){
+		
+		//System.setSecurityManager(new SecurityManager());
+		try{
+			System.setProperty("java.rmi.server.hostname","localhost");
+			RemoteShapeList shapeList = new RemoteShapeListServant();
+			//Registry registry = LocateRegistry.getRegistry();
+			Registry registry = LocateRegistry.createRegistry(6000);
+			registry.bind("shapeList", shapeList);
+			System.out.println("server ready");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// https://stackoverflow.com/questions/15685686/java-rmi-connectexception-connection-refused-to-host-127-0-1-1
+	}
+}
