1a8ab2ed2f52d84648a8ee8d7211b4ac6a3850fa
hAhmadz <haaris.ahmad1@gmail.com>
2017-10-16 22:38:31 +1100

    Major changes.. Every1 verify. No pushing of any other files apart from src folder

---
+++ src/Server/RemoteShapeList.java
@@ -0,0 +1,39 @@
+package Server;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import Misc.ColoredShape;
+
+import Client.ClientEx;
+import Client.ClientExInt;
+
+public interface RemoteShapeList extends Remote
+{
+    public void addColoredShape(ColoredShape shape) throws RemoteException;
+
+    public void clear() throws RemoteException;
+
+    public void newDiagram() throws RemoteException;
+
+    public void undoDrawing() throws RemoteException;
+
+    public void redoDrawing() throws RemoteException;
+
+    public void saveDrawing(String filename) throws RemoteException;
+
+    public void openDrawing(String filename) throws RemoteException;
+
+    public ArrayList<String> messageStream(String message) throws RemoteException;
+
+    public ArrayList<String> getMsgs() throws RemoteException;
+    
+    public boolean subscribe(ClientExInt clientEx) throws RemoteException;
+    
+    // public boolean subscribeChat(MessagingInt chatClient) throws RemoteException;
+    
+    public void publishChat() throws RemoteException;
+    
+    public void addMessage(String message) throws RemoteException;
+    
+}
